0b2e85822569c2f749b036ebd0b180b5ba07b4d7
Brandon Main <brandonlmain@gmail.com>
2020-11-17 13:47:52 -0500

    added create model logic

--- src/app/components/analytical/analytical.module.ts
+++ src/app/components/analytical/analytical.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { AnalyticalComponent } from "./analytical.component";
 import { AnalyticalModelsComponent } from './analytical-models/analytical-models.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from "@angular/material/button";
 import { CreateModelComponent } from './create-model/create-model.component';
 
 @NgModule({
   declarations: [
     AnalyticalComponent, 
-    AnalyticalModelsComponent, CreateModelComponent
+    AnalyticalModelsComponent,
+    CreateModelComponent
   ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule
   ],
   exports: [
     AnalyticalComponent
