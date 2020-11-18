024e6a5cb9d3f6199aac83f5c1b68aa587a43481
Brandon Main <brandonlmain@gmail.com>
2020-11-18 12:19:56 -0500

    added stepper to create model

--- src/app/components/analytical/analytical.module.ts
+++ src/app/components/analytical/analytical.module.ts
@@ -4,6 +4,8 @@ import { AnalyticalComponent } from "./analytical.component";
 import { AnalyticalModelsComponent } from './analytical-models/analytical-models.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from "@angular/material/button";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatSelectModule } from "@angular/material/select";
 import { CreateModelComponent } from './create-model/create-model.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
@@ -19,6 +21,8 @@ import { MatInputModule } from "@angular/material/input";
     MatTableModule,
     MatButtonModule,
     MatInputModule,
+    MatStepperModule,
+    MatSelectModule,
     FormsModule, 
     ReactiveFormsModule
   ],
--- src/app/components/analytical/create-model/create-model.component.ts
+++ src/app/components/analytical/create-model/create-model.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-model',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateModelComponent implements OnInit {
 
-  constructor() { }
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
 
-  ngOnInit(): void {
+  types : string[] = [
+    "Linear Regression", 
+    "Partial Least Squares", 
+    "Gradient Boosting Machine"
+  ];
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+      fourthCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required]
+    });
   }
 
 }
