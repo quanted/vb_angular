3ace70c68d0d9560d7509c287c2d6881e74e9fd8
Gavin <jasondunken@yahoo.com>
2021-03-02 23:40:02 -0500

    removed unused code from auth interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,28 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse, JsonpClientBackend } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
 import { AuthService } from 'src/app/services/auth.service';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
   constructor(public auth: AuthService) {}
   
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
     if (!request.body?.password) {
       request = request.clone({ setHeaders: { Authorization: `Token ${this.auth.getToken()}` } });
     }
-
-    return next.handle(request).pipe(
-      map((event: HttpEvent<any>) => {
-          if (event instanceof HttpResponse) {
-              // console.log('event--->>>', event);
-          }
-          return event;
-      }));
+    return next.handle(request);
   }
 }
