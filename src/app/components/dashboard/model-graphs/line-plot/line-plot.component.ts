bd64a59ac3a84685dce323e95e215ffd270a457c
Brandon Main <brandonlmain@gmail.com>
2021-04-29 16:19:07 -0400

    updated dashboard

--- src/app/components/dashboard/model-graphs/line-plot/line-plot.component.ts
+++ src/app/components/dashboard/model-graphs/line-plot/line-plot.component.ts
@@ -6,37 +6,35 @@ import * as d3 from 'd3';
   templateUrl: './line-plot.component.html',
   styleUrls: ['./line-plot.component.css']
 })
-export class LinePlotComponent implements OnInit, OnChanges {
+export class LinePlotComponent implements OnInit {
 
-  @ViewChild('chart', { static: true }) protected chartContainer: ElementRef;
   @Input() data: any;
   svg: any;
-  g: any;
-  tooltip: any;
-  margin: { top: number; right: number; bottom: number; left: number; };
+  margin = { top: 30, right: 30, bottom: 30, left: 30 };
   padding: { top: number; right: number; bottom: number; left: number; };
-  contentWidth: number;
-  contentHeight: number;
-  width: number;
-  height: number;
+  dataKeys: string[];
+  chartData = [];
+  width;
+  height;
+  scaleX;
+  scaleY;
 
-  constructor() { }
-
-  ngOnInit(): void {}
-
-
+  ngOnInit(): void {
+    this.initChart();
+    this.createChart();
+  }
 
   private initChart(): void {
-    const element = this.chartContainer.nativeElement;
+    // const element = this.chartContainer.nativeElement;
 
-    this.svg = d3.select(element);
+    this.svg = d3.select('svg');
 
-    this.margin = {
-      top: this.svg.style('margin-top').replace('px', ''),
-      right: this.svg.style('margin-right').replace('px', ''),
-      bottom: this.svg.style('margin-bottom').replace('px', ''),
-      left: this.svg.style('margin-left').replace('px', '')
-    };
+    // this.margin = {
+    //   top: this.svg.style('margin-top').replace('px', ''),
+    //   right: this.svg.style('margin-right').replace('px', ''),
+    //   bottom: this.svg.style('margin-bottom').replace('px', ''),
+    //   left: this.svg.style('margin-left').replace('px', '')
+    // };
 
     this.padding = {
       top: this.svg.style('padding-top').replace('px', ''),
@@ -48,64 +46,49 @@ export class LinePlotComponent implements OnInit, OnChanges {
     this.width = this.svg.style('width').replace('px', '');
     this.height = this.svg.style('height').replace('px', '');
 
-    this.contentWidth = this.width - this.margin.left - this.margin.right - this.padding.left - this.padding.right;
-    this.contentHeight = this.height - this.margin.top - this.margin.bottom;
+    this.svg.append('g').attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
-    this.g = this.svg.append('g').attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+    for (let i = 0; i < this.data.y.length; i++) {
+      this.chartData.push({ x: this.data.x[i], y: this.data.y[i]});
+    }
+
+    console.log('chartData: ', this.chartData);
+    this.scaleX = d3.scalePoint()
+      .domain(this.data.x)
+      .range([this.margin.left, this.width - this.margin.right]);
+
+    this.svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(' + 0 + ',' + (this.height - this.margin.bottom) + ')')
+      .call(d3.axisBottom(this.scaleX)); // Create an axis component with d3.axisBottom
+
+    this.scaleY = d3.scaleLinear()
+      .domain(d3.extent(this.chartData, d =>  d.y))
+      .range([this.height - this.margin.bottom, this.margin.top]);
+
+    this.svg.append('g')
+      .attr('class', 'y axis')
+      .attr('transform', 'translate(' + this.margin.left + ', 0)')
+      .call(d3.axisLeft(this.scaleY));
   }
 
   private createChart(): void {
-    // Add title
-    this.svg.append('text')
-      .attr('x', (this.contentWidth / 2))
-      .attr('y', 0 - (this.margin.bottom / 2))
-      .attr('text-anchor', 'middle')
-      .style('font-size', '16px')
-      .text('elastic-net neg_mean_absolute_error');
-
-    // Add X axis
-    const x = d3.scaleLinear()
-      .domain([1, this.data.y.length])
-      .range([ 0, this.contentWidth ]);
-    this.g.append('g')
-      .attr('class', 'x axis')
-      .attr('transform', 'translate(' + 0 + ',' + this.contentHeight + ')')
-      .call(d3.axisBottom(x)); // Create an axis component with d3.axisBottom
-
-    // Add Y axis
-    const y = d3.scaleLinear()
-      .domain([Math.min(...this.data.y),
-        Math.max(...this.data.y) + 1])
-      .range([this.contentHeight, 0]);
-    this.g.append('g')
-      .attr('class', 'y axis')
-      // .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
-      .call(d3.axisLeft(y)); // Create an axis component with d3.axisLeft
-
-    // Add y data
-    let index = 0;
-    const lines = this.g.append('path')
-      .datum(this.data.y)
+    d3.select('svg')
+      .style('background', 'none')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .style('display', 'block')
+      .append('path')
+      .datum(this.chartData)
       .attr('fill', 'none')
       .attr('stroke', 'steelblue')
       .attr('stroke-width', 1.5)
       .attr('d', d3.line()
-        .x(() => x(index += 1))
-        .y((d) => {
-          return y(Number(d));
+        .x((d) => {
+          // console.log(this.scaleX(d['date']));
+          return this.scaleX(d['x']);
         })
+        .y((d) => this.scaleY(d['y']))
       );
-
-    // Add labels
-    lines.selectAll('text')
-      .data(this.data)
-      .enter();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (this.data) {
-      this.initChart();
-      this.createChart();
-    }
   }
 }
--- src/app/components/dashboard/model-graphs/model-graphs.component.ts
+++ src/app/components/dashboard/model-graphs/model-graphs.component.ts
@@ -9,11 +9,26 @@ import * as data from '../../../../../test_data/project_cv_results.json';
 export class ModelGraphsComponent implements OnInit {
 
   public data: any;
-  public loop;
+  graphData = {
+    x: [],
+    y: []
+  };
   constructor() { }
 
   ngOnInit(): void {
-    this.loop = new Array(4);
     this.data = data;
+    this.graphData.x = [...Array(10)].map((_, i) => 1 + i);
+    this.graphData.y = this.data.default.cv_score['elastic-net'].r2;
+  }
+
+  /**
+   * Average across columns of arrays into a single array of averages.
+   * @param dataArray
+   */
+  averageYHat(dataArray) {
+    return dataArray.reduce((acc, cur) => {
+      cur.forEach((e, i) => acc[i] = acc[i] ? acc[i] + e : e);
+      return acc;
+    }, []).map(e => e / dataArray.length);
   }
 }
