4c2429e883e1dd894a645728a223fa33e8160cfa
Brandon Main <brandonlmain@gmail.com>
2021-05-05 14:38:13 -0400

    added dashboard service to reduce data manip overhead

---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  data: any;
+  negMeanAbsoluteError = [];
+  negMeanSquaredError = [];
+
+  constructor() { }
+
+  /**
+   * The data is supplied here as a variable and formatted
+   * for chart consumption.
+   */
+  parseData(data: any) {
+    this.data = data;
+    this.setCVScores();
+  }
+
+  /**
+   *
+   */
+  setCVScores() {
+
+    // Doing this for now but updating this method soon.
+    //
+    //
+    for (const pipe in this.data.cv_score) {
+      const selected = 'neg_mean_absolute_error';
+      if (this.data.cv_score.hasOwnProperty(pipe)) {
+        for (let i = 0; i < this.data.cv_score[pipe][selected].length; i++) {
+          this.negMeanAbsoluteError.push({
+            type: pipe,
+            x: i + 1,
+            y: this.data.cv_score[pipe][selected][i]
+          });
+        }
+      }
+    }
+
+    for (const pipe in this.data.cv_score) {
+      const selected = 'neg_mean_squared_error';
+      if (this.data.cv_score.hasOwnProperty(pipe)) {
+        for (let i = 0; i < this.data.cv_score[pipe][selected].length; i++) {
+          this.negMeanSquaredError.push({
+            type: pipe,
+            x: i + 1,
+            y: this.data.cv_score[pipe][selected][i]
+          });
+        }
+      }
+    }
+  }
+}
