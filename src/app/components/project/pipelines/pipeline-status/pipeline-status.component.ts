861c44701a5ca062f5f7b79dd530e9c1afb64c99
jasondunken <jasondunken@yahoo.com>
2022-03-26 13:31:57 -0400

    refactor pipeline-status component

--- src/app/components/project/pipelines/pipeline-status/pipeline-status.component.ts
+++ src/app/components/project/pipelines/pipeline-status/pipeline-status.component.ts
@@ -1,13 +1,76 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, OnDestroy } from "@angular/core";
+import { PipelineService } from "src/app/services/pipeline.service";
 
 @Component({
     selector: "app-pipeline-status",
     templateUrl: "./pipeline-status.component.html",
     styleUrls: ["./pipeline-status.component.css"],
 })
-export class PipelineStatusComponent implements OnInit {
-    @Input() pipelines;
-    constructor() {}
+export class PipelineStatusComponent implements OnInit, OnDestroy {
+    @Input() project;
+    pipelines = [];
 
-    ngOnInit(): void {}
+    panelOpenState = false;
+
+    pipelinesStatusMessage = "";
+
+    pipelineUpdateTimer: ReturnType<typeof setInterval>;
+
+    constructor(private pipelineService: PipelineService) {}
+
+    ngOnInit(): void {
+        // FOR TESTING
+        this.project["executed"] = false;
+        this.project["completed"] = false;
+        //need to move this so it only runs if there are pipelines executing
+        this.updatePipelines();
+        if (this.project.executed && !this.project.completed) {
+            this.pipelineUpdateTimer = setInterval(() => {
+                this.updatePipelines();
+            }, 5000);
+        }
+    }
+
+    updatePipelines() {
+        this.pipelinesStatusMessage = "Updating status...";
+        this.pipelineService.getProjectPipelines(this.project.id).subscribe((pipelines) => {
+            if (pipelines.length > 0) {
+                console.log("pipelines: ", pipelines);
+                this.pipelines = [...pipelines];
+
+                let pipelinesCompleted = true;
+
+                for (let pipeline of this.pipelines) {
+                    const pipelineStatus = pipeline.metadata.status;
+                    if (pipelineStatus) {
+                        this.pipelinesStatusMessage = "Pipelines executing...";
+
+                        if (pipelineStatus !== "Completed and model saved") {
+                            pipelinesCompleted = false;
+                        } else {
+                            pipeline["completed"] = true;
+                        }
+                    } else {
+                        this.pipelinesStatusMessage = "Pipeline(s) unexecuted";
+                        pipelinesCompleted = false;
+                    }
+                }
+
+                if (pipelinesCompleted) {
+                    this.pipelinesStatusMessage = "Pipeline execution complete";
+                    clearInterval(this.pipelineUpdateTimer);
+                }
+            } else {
+                this.pipelinesStatusMessage = "No pipelines";
+            }
+        });
+    }
+
+    cancelPipeline(): void {
+        console.log("cancel pipeline not implemented");
+    }
+
+    ngOnDestroy() {
+        clearInterval(this.pipelineUpdateTimer);
+    }
 }
--- src/app/components/project/project-detail/project-detail.component.ts
+++ src/app/components/project/project-detail/project-detail.component.ts
@@ -1,118 +1,67 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Router } from '@angular/router';
-import { DatasetService } from 'src/app/services/dataset.service';
-import { LocationService } from 'src/app/services/location.service';
-import { PipelineService } from 'src/app/services/pipeline.service';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { DatasetService } from "src/app/services/dataset.service";
+import { LocationService } from "src/app/services/location.service";
 
-import { ProjectService } from 'src/app/services/project.service';
+import { ProjectService } from "src/app/services/project.service";
 
 @Component({
-  selector: 'app-project-detail',
-  templateUrl: './project-detail.component.html',
-  styleUrls: ['./project-detail.component.css']
+    selector: "app-project-detail",
+    templateUrl: "./project-detail.component.html",
+    styleUrls: ["./project-detail.component.css"],
 })
 export class ProjectDetailComponent implements OnInit {
-  panelOpenState = false;
-  
-  @Input() project;
-  @Output() projectDeleted: EventEmitter<any> = new EventEmitter<any>();
+    @Input() project;
+    @Output() projectDeleted: EventEmitter<any> = new EventEmitter<any>();
 
-  locationName = '';
-  datasetName = '';
-  pipelines = [];
-  pipelinesStatusMessage = '';
+    locationName = "";
+    datasetName = "";
+    pipelines = [];
 
-  pipelineUpdateTimer;
-  hasDashboard = false;
+    constructor(
+        private router: Router,
+        private projectService: ProjectService,
+        private locationService: LocationService,
+        private datasetService: DatasetService
+    ) {}
 
-  constructor(
-    private router: Router,
-    private projectService: ProjectService,
-    private locationService: LocationService,
-    private datasetService: DatasetService,
-    private pipelineService: PipelineService
-  ) { }
-
-  ngOnInit(): void {
-    this.locationService.getLocations().subscribe(locations => {
-      for (let location of locations) {
-        if (location.id === this.project.location) {
-          this.locationName = location.name;
+    ngOnInit(): void {
+        this.locationService.getLocations().subscribe((locations) => {
+            for (let location of locations) {
+                if (location.id === this.project.location) {
+                    this.locationName = location.name;
+                }
+            }
+        });
+        if (this.project.dataset) {
+            this.datasetService.getDataset(this.project.dataset).subscribe((dataset) => {
+                this.datasetName = dataset.name;
+            });
         }
-      }
-    });
-    if (this.project.dataset) {
-      this.datasetService.getDataset(this.project.dataset).subscribe(dataset => {
-        this.datasetName = dataset.name;
-      })
     }
 
-    //need to move this so it only runs if there are pipelines executing
-    this.updatePipelines();
-    this.pipelineUpdateTimer = setInterval(() => {
-      this.updatePipelines();
-    }, 5000);
-  }
+    editProject(project) {
+        this.router.navigateByUrl(`project/${project.id}`);
+    }
 
-  ngOnDestroy() {
-    clearInterval(this.pipelineUpdateTimer);
-  }
+    executeProject(project): void {
+        this.projectService.executeProject(project);
+    }
 
-  editProject(project) {
-    this.router.navigateByUrl(`project/${project.id}`);
-  }
+    gotoDashboard(project) {
+        this.router.navigateByUrl(`dashboard/${project.id}`);
+    }
 
-  gotoDashboard(project) {
-    this.router.navigateByUrl(`dashboard/${project.id}`);
-  }
+    cloneProject(project) {
+        this.projectService.cloneProject(project).subscribe((response) => {
+            console.log("clone: ", response);
+            this.router.navigateByUrl(`project/${response.id}`);
+        });
+    }
 
-  cloneProject(project) {
-    console.log("project cloning not implemented yet!");
-  }
-  
-  deleteProject(project) {
-    this.projectService.deleteProject(project.id).subscribe(() => {
-      this.projectDeleted.emit();
-    });
-  }
-
-  updatePipelines() {
-    this.pipelinesStatusMessage = "Updating status...";
-    this.pipelineService.getProjectPipelines(this.project.id).subscribe((pipelines) => {
-      if (pipelines.length > 0){
-        console.log('pipelines: ', pipelines);
-        this.pipelines = [...pipelines];
-
-        let pipelinesCompleted = true;
-
-        for (let pipeline of this.pipelines) {
-          const pipelineStatus = pipeline.metadata.status;
-          if  (pipelineStatus) {
-            this.pipelinesStatusMessage = "Pipelines executing..."
-
-            if (pipelineStatus !== "Completed and model saved") {
-              pipelinesCompleted = false;
-            } else {
-              pipeline["completed"] = true;
-            }
-            
-          } else {
-            this.pipelinesStatusMessage = "Pipeline(s) unexecuted";
-            pipelinesCompleted = false;
-          }
-        }
-
-        if (pipelinesCompleted) {
-          this.pipelinesStatusMessage = "Pipeline execution complete"
-          this.hasDashboard = true;
-          clearInterval(this.pipelineUpdateTimer);
-        }
-      } else {
-        this.pipelinesStatusMessage = "No pipelines"}
-    })
-  }
-
-  cancelPipeline(): void {
-    console.log("cancel pipeline not implemented");
-  }
+    deleteProject(project) {
+        this.projectService.deleteProject(project.id).subscribe(() => {
+            this.projectDeleted.emit();
+        });
+    }
 }
