fb8467395d07032010ded20795f60c7cb6befff2
jasondunken <jasondunken@yahoo.com>
2022-03-30 13:32:10 -0400

    update home and welcome styling

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,47 +1,41 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
-import { ProjectService } from 'src/app/services/project.service';
+import { ProjectService } from "src/app/services/project.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  
-  statusMessage: string;
+    statusMessage: string = "";
 
-  projects;
+    projects: any = [];
 
-  constructor(
-    private projectService: ProjectService,
-    private router: Router) {}
+    constructor(private projectService: ProjectService, private router: Router) {}
 
-  ngOnInit() {
-    this.statusMessage = '';
-    
-    this.projects = [];
-    this.projectService.getProjects().subscribe((projects) => {
-      if (!projects.error) {
-        if (projects.length < 1) {
-          this.statusMessage = "You have no stored projects";
-        } else {
-          this.projects = [...projects];
-        }
-      } else {
-        console.log(projects.error);
-      }
-    });
-  }
-  
-  createProject(): void {
-    this.router.navigateByUrl("project");
-  }
+    ngOnInit() {
+        this.projectService.getProjects().subscribe((projects) => {
+            if (!projects.error) {
+                if (projects.length < 1) {
+                    this.statusMessage = "You have no stored projects";
+                } else {
+                    this.projects = [...projects];
+                }
+            } else {
+                console.log(projects.error);
+            }
+        });
+    }
 
-  projectDeleted(): void {
-    this.projectService.getProjects().subscribe((projects) => {
-      this.projects = [...projects];
-    });
-  }
+    createProject(): void {
+        this.router.navigateByUrl("project");
+    }
+
+    projectDeleted(): void {
+        this.projectService.getProjects().subscribe((projects) => {
+            this.projects = [...projects];
+        });
+    }
 }
